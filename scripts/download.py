8d01a8c7d58e48241a303163644930f3c900346b
vincent d warmerdam <vincentwarmerdam@gmail.com>
2023-07-11 12:53:42 +0000

    this should finally fix the download issue

--- scripts/download.py
+++ scripts/download.py
@@ -4,15 +4,18 @@ It's just that this script adds a retry mechanic and has less dependencies
 so that it's more lightweight to run from Github Actions.
 """
 
-from retry import retry 
+import logging
 import datetime as dt 
+from pathlib import Path
+from typing import List
+
 import srsly
 import tqdm
 import arxiv
 from arxiv import Result
+from retry import retry 
 import spacy
 from spacy.language import Language
-from typing import List
 from pydantic import BaseModel 
 
 
@@ -38,10 +41,10 @@ def parse(res: Result, nlp: Language) -> ArxivArticle:
     
     return ArxivArticle(
         created=str(res.published)[:19], 
-        title=res.title,
+        title=str(res.title),
         abstract=summary,
         sentences=sents,
-        url=res.entry_id
+        url=str(res.entry_id)
     )
 
 @retry(tries=5, delay=1, backoff=2)
@@ -56,13 +59,14 @@ def main():
 
     results = list(items.results())
 
-    articles = [parse(r, nlp=nlp) 
+    articles = [dict(parse(r, nlp=nlp)) 
                 for r in tqdm.tqdm(results) 
                 if total_seconds(r) < 2.5 and r.primary_category.startswith("cs")]
 
     filename = str(dt.datetime.now()).replace(" ", "-")[:13] + "h.jsonl"
-    srsly.write_jsonl(filename, articles)
+    srsly.write_jsonl(Path("downloads") / filename, articles)
 
 
 if __name__ == "__main__":
+    logging.basicConfig()
     main()
